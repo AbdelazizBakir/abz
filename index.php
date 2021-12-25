c937c58bc1b0f9453a719d6ebe5e93780b16ad07
ABDELAZIZ Bakir <abdelaziz-bakir@outlook.fr>
2021-12-25 09:33:11 +0100

    ABDELAZIZ Bakir

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Custom style -->
-    <link href="assets/carousel/carousel.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
   </head>
           <!--Début-->
@@ -58,7 +57,7 @@
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-inner">
+    <!--<div class="carousel-inner">
       <div class="carousel-item active">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
@@ -92,7 +91,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>-->
     <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
