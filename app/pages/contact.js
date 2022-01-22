7687e4b05e4ea03c594e012c3b986c43e9938e0a
AbdelazizBakir <abdelaziz-bakir@outlook.fr>
2022-01-22 11:59:38 +0100

    By abz

---
+++ app/pages/contact.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function about() {
+  return (
+    <> 
+    <h1>contact page</h1>
+  </>
+  )
+}
